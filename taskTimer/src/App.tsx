8130978729d1e8af56ccf1120bb4b7de1a3f3f30
Samuel Roberto <samucada160@gmail.com>
2024-03-21 15:25:37 -0300

    styled-components

--- taskTimer/src/App.tsx
+++ taskTimer/src/App.tsx
@@ -1,10 +1,15 @@
+import { Button } from "./components/Button";
 
 export function App() {
 
   return (
-    <h1>Hello World
-      Olá mundo
-    </h1>
+    <>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+      <Button />
+    </>
   )
 }
 
