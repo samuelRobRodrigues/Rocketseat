b5535a680e0aebf081a2c6a81228987b5061bbde
Samuel Roberto <samucada160@gmail.com>
2024-03-27 23:33:04 -0300

    att

--- taskTimer/src/components/Button.styles.ts
+++ taskTimer/src/components/Button.styles.ts
@@ -18,10 +18,12 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
 
   background-color: ${props => props.theme.primary};
   color: ${props => props.theme.white};
-  /* ${props => {
-    return css`
-      background-color: ${buttonVariants[props.variant]}
-    `
-  }}
-  }} */
+
+
+    /* ${props => {
+      return css`
+        background-color: ${buttonVariants[props.variant]}
+      `
+    }}
+    }} */
 `;
--- taskTimer/src/styles/themes/default.ts
+++ taskTimer/src/styles/themes/default.ts
@@ -2,5 +2,4 @@ export const defaultTheme = {
     primary: 'purple',
     secondary: 'orange',
     white: '#fff',
-    black: '#000'
 }
