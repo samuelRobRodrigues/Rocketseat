85a6fea75fb5b6bad0b35b70a3d0814c13feba70
Samuel Roberto <samucada160@gmail.com>
2024-04-02 06:44:30 -0300

    remove-recap

--- taskTimer/src/@types/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components';
-import { defaultTheme } from '../styles/themes/default';
-
-type themeType = typeof defaultTheme;
-
-declare module 'styled-components' {
-    export interface DefaultTheme extends themeType {}
-}
